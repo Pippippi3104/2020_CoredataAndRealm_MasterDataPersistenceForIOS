c6782d3c20156b0a0d95c2d3c176142213066d24
Shimamura Satoshi <shimamura.satoshi.jri@outlook.jp>
2020-11-30 14:32:08 +0900

    # note the progress -01

---
+++ src/Todoey/Todoey/TodoeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoeyApp.swift
+//  Todoey
+//
+//  Created by 推進 dx on 2020/11/30.
+//
+
+import SwiftUI
+
+@main
+struct TodoeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
