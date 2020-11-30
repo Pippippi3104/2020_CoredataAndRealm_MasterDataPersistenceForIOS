766237346f5eb6a9039adf5147f52c8556e37e1e
Shimamura Satoshi <shimamura.satoshi.jri@outlook.jp>
2020-11-30 15:07:14 +0900

    # note the progress - 05

--- src/Todoey/Todoey/TodoListViewController.swift
+++ src/Todoey/Todoey/TodoListViewController.swift
@@ -16,5 +16,18 @@ class TodoListViewController: UITableViewController {
     }
     
     //MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+    UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
 }
 
