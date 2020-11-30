de75734a85e0c6d37bf9a640967c9e32498d47b3
Shimamura Satoshi <shimamura.satoshi.jri@outlook.jp>
2020-11-30 14:33:49 +0900

    # note the progress - 02

---
+++ src/Todoey/Todoey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by 推進 dx on 2020/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
