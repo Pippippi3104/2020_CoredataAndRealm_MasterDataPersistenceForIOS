44aa9773ae44701e132b1400af6729bfdffd8468
Shimamura Satoshi <shimamura.satoshi.jri@outlook.jp>
2020-11-30 16:00:54 +0900

    # note the progress - 04

--- src/Todoey/Todoey/TodoListViewController.swift
+++ src/Todoey/Todoey/TodoListViewController.swift
@@ -33,7 +33,13 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(indexPath.row)
-        print(itemArray[indexPath.row])
+        //print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
