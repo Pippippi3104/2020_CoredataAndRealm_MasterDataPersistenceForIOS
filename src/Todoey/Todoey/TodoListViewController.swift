a76f37962840bda89f7e447ce7d5a5fcf45163ac
Shimamura Satoshi <shimamura.satoshi.jri@outlook.jp>
2020-11-30 14:42:37 +0900

    # note the progress - 03

--- src/Todoey/Todoey/TodoListViewController.swift
+++ src/Todoey/Todoey/TodoListViewController.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
 
