3e4fe3b64050d585a8f13749aede26784904883a
Shimamura Satoshi <shimamura.satoshi.jri@outlook.jp>
2020-11-30 16:01:47 +0900

    # finish editing lesson03

--- src/Todoey/Todoey/TodoListViewController.swift
+++ src/Todoey/Todoey/TodoListViewController.swift
@@ -32,9 +32,6 @@ class TodoListViewController: UITableViewController {
     //MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(indexPath.row)
-        //print(itemArray[indexPath.row])
-        
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         } else {
