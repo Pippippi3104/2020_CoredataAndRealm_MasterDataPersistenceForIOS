2918a957d35fa07ceb3dddec8298e8fbc154ee60
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2020-12-05 15:24:10 +0900

    # note the progress - 02

--- src/Practice/MyPlayground.playground/Contents.swift
+++ src/Practice/MyPlayground.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
 var str = "Hello, playground"
+
+let defaults = UserDefaults.standard
+
+defaults.set(0.24, forKey: "Volume")
+defaults.set(true, forKey: "MusicOn")
+defaults.set("Angela", forKey: "PlayerName")
+defaults.set(Data(), forKey: "AppLastOpenedByUser")
+
+let volume = defaults.float(forKey: "Volume")
+let appLastOpen = defaults.object(forKey: "AppLastOpendByUser")
